f5bf95f9a974eef2b37d96fea1624bb6c7dfc39e
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-14 12:16:53 -0700

    Generate Comment model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_173751) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_191617) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "link"
     t.string "caption"
