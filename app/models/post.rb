6c1e8593a7e51a83fb58ed0dd465e228ecd0d893
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-14 12:24:16 -0700

    Add associations between User, Post and Comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :link, presence: true, length: { maximum: 140 }
   validates :caption, length: { maximum: 140 }
   belongs_to :user
+  has_many :comments
 end
