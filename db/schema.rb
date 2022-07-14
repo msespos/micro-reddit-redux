29b4252369c20b56a9ffdead558aa0d283e60bb0
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-14 10:45:39 -0700

    Add user_id to posts and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_164314) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_173751) do
   create_table "posts", force: :cascade do |t|
     t.string "link"
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
