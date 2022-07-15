4574bfc7fe9582f5e30683d5ed7809e51bee4302
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-15 10:48:24 -0700

    Limit post routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
-  resources :posts do
+  resources :posts, only: :index do
     collection do
       post 'vote'
     end
