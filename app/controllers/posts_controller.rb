acb870d651c7a89a06079041d43843886bdf30e3
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-14 18:56:03 -0700

    Add vote action and vote_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def vote
+    @vote = Vote.new(vote_params)
+    @vote.user_id = current_user.id
+    if @vote.save
+      flash[:notice] = 'You voted on a post!'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Error - could not process vote'
+      render 'new'
+    end
+  end
+
+  private
+
+    def vote_params
+      params.require(:vote).permit(:upvote, :user_id, :post_id,)
+    end
 end
