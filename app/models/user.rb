24c382448cdab8038b3a4d010ed32d2e731bf4da
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-15 14:21:54 -0700

    Comment out pre-devise validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  # validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   has_many :posts
   has_many :comments
   has_many :votes
