e3e016c823aedb6cc908243ed7e8ebbe2744c21c
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-15 18:52:52 -0700

    Remove get users to registrations line

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   devise_scope :user do
     root to: 'devise/sessions#new'
-    get '/users', to: 'devise/registrations#new'
   end
 
   resources :posts, only: :index do
