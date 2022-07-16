2242a87dde1d929591ac24d4c1d33ee8a674a707
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-16 08:55:14 -0700

    Edit root route to go to Posts index page, not session login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  root to: 'posts#index'
 
   resources :posts, only: [:index, :new, :create] do
     collection do
